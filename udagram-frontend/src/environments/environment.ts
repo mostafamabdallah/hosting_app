0579357e382650e4310002236320a31c60fb6aed
Mostafa Mahmoud Abdallah <mostafamabdallah94@gmail.com>
2022-03-07 15:10:26 +0200

    9 commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagramapp-env.eba-iagyku8k.us-east-1.elasticbeanstalk.com/v0'
 };
 
 /*
