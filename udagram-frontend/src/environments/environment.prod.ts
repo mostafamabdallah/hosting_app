a6576721a6c9bb0e726f56dd8bac648dbfb30723
Mostafa Mahmoud Abdallah <mostafamabdallah94@gmail.com>
2022-03-08 16:57:22 +0200

    14 commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "udagram-app",
-  apiHost: "http://udagramapp-env.eba-iagyku8k.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagramapp-env.eba-5zqacb78.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramapp-env.eba-iagyku8k.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagramapp-env.eba-5zqacb78.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
